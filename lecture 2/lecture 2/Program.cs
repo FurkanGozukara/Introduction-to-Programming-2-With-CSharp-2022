741417e5a8d235d2401c199b2cf7b2210a6ce0f8
Furkan Gözükara <furkangozukara@gmail.com>
2022-03-08 16:16:17 +0300

    lecture 2 part 4

--- lecture 2/lecture 2/Program.cs
+++ lecture 2/lecture 2/Program.cs
@@ -16,6 +16,42 @@ namespace Lecture2 // Note: actual namespace depends on the project name.
 
             //modify your method to handle this input gracefully as well
             var vrResult2 = exClass1.aSpecialMethod("the value of ; 2a.2^a5");
+
+            //method overloading
+            var vr1 = customSum(32.12, 22, 51);
+            var vr2 = customSum(11, 0.22, 33);//double+double+int or int+double+int
+            var vr4 = customSum(11d, 0.22, 33); // or var vr4 = customSum(11.0, 0.22, 33);
+            var vr3 = customSum("11", 43, "35");
+        }
+
+        //double + int + int
+        public static double customSum(double dbl1, int ir1, int ir2)
+        {
+            return dbl1 + ir1 + ir2;   
+        }
+
+        //int + double + int
+        public static double customSum(int ir1, double dbl2, int ir2)
+        {
+            return ir1 + dbl2 + ir2;
+        }
+
+        //double + double + int
+        public static double customSum(double dbl1, double dbl2, int ir2)
+        {
+            return dbl1 + dbl2 + ir2;
+        }
+
+        //string + double + string
+        public static string customSum(string str1, double dbl2, string str2)
+        {
+            return str1 + dbl2 + str2;
+        }
+
+        //string + string + double
+        public static string customSum(string str12, string str22 , double dbl22)
+        {
+            return str12 + dbl22 + str22;
         }
 
         public long sumTwoNumbers_nonStatic(long number1, long number2)
@@ -64,7 +100,7 @@ namespace Lecture2 // Note: actual namespace depends on the project name.
             //    Console.WriteLine($"an exception occured when parsing double value. the exception message is " + E.Message + "\r\n\r\n" + E.StackTrace);
             //}
 
-            bool blTryParseResult = double.TryParse(vrSplit[1].Split('^').FirstOrDefault(), out dblBase);
+            bool blTryParseResult = double.TryParse(vrSplit[1].Split('^').FirstOrDefault(), out dblBase);//if try parse fails, the ouput will become default value and default value of double is 0
 
             Console.WriteLine($"the double conversation of {vrSplit[1].Split('^').FirstOrDefault()} resulted in : {blTryParseResult.ToString()} and the result is {dblBase.ToString()}");
 
