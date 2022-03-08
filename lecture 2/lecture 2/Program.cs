735bfa9b1f72c0b62b00539faeebe86c3683d11b
Furkan Gözükara <furkangozukara@gmail.com>
2022-03-08 13:37:40 +0300

    lecture 2 part 1

---
+++ lecture 2/lecture 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lecture2 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public long sumTwoNumbers_nonStatic(long number1, long number2)
+        {
+            return number1 + number2;
+        }
+
+        public static long sumTwoNumbers_Static(long number1, long number2)
+        {
+            return number1 + number2;
+        }
+    }
+
+    public static class exClass1
+    {
+        public static void doStuff()
+        {
+            //Program.sumTwoNumbers_nonStatic(32,11);//to fix this we need to change like below
+            Program myProgram = new Program();
+            myProgram.sumTwoNumbers_nonStatic(32, 11);
+
+            Program.sumTwoNumbers_Static(32, 11);
+        }
+    }
+}
