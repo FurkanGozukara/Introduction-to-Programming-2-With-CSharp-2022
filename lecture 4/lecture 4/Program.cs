239550ff05c750ad96127968c3a9d33e8e3210e9
Furkan Gözükara <furkangozukara@gmail.com>
2022-03-22 14:30:48 +0300

    lecture 4 part 3

--- lecture 4/lecture 4/Program.cs
+++ lecture 4/lecture 4/Program.cs
@@ -7,8 +7,72 @@ namespace lecture_1503
     {
         static void Main(string[] args)
         {
-            lecture_4.customMethods.AskAnimals();
-            customMethods.AskAnimals();
+            doThings();
+
+            //lecture_4.customMethods.AskAnimals();
+            // customMethods.AskAnimals();
+
+            customMethods myMethods = new customMethods();
+            myMethods.AskAnimalsV2();
+
+        }
+
+        static void doThings()
+        {
+            Random random = new Random();
+            List<int> listOfnumbers = new List<int>();
+            int rnd;
+
+            for (int i = 0; i < 2000; i++)
+            {
+                rnd = random.Next(0, 2000);
+                listOfnumbers.Add(rnd);
+
+            }
+            returnList(listOfnumbers);
+            Console.WriteLine("Returns this");
+            foreach (var item in listOfnumbers)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+        public static void returnList(List<int> numbersList)
+        {
+            var vrSecondList = numbersList.ToList();//deep clone
+            var vrList2 = numbersList;//this wont fix enumaration collection error //this is shallow clone
+
+            foreach (var vrPerNumber in vrSecondList)
+            {
+                if (IsPrime(vrPerNumber) == false)
+                {
+                    numbersList.Remove(vrPerNumber);
+                }
+            }
+
+            for (int i = 0; i < vrSecondList.Count; i++)//this uses lesser memory
+            {
+                if (IsPrime(vrSecondList[i]) == false)
+                {
+                    vrSecondList.RemoveAt(i);
+                    i--;
+                }
+            }
+        }
+
+        public static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+                if (number % i == 0)
+                    return false;
+
+            return true;
         }
 
     }
