6703aa9856f4c6e04453ae66330a44c8a8917acf
Furkan Gözükara <furkangozukara@gmail.com>
2022-04-26 13:55:36 +0300

    lecture 8 part 1

---
+++ lecture 8/lecture 7/DbOperations.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lecture_7
+{
+    public static class DbOperations
+    {
+        //to learn your server name execute this command select @@servername
+        private static string srConnectionString = @"server=DESKTOP-ULH4M26;database=School; integrated security=SSPI;persist security info=False; Trusted_Connection=Yes;";
+
+        public static DataTable selectTable(string srQuery)
+        {
+            DataTable dtReturnTable = new DataTable();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(srConnectionString))
+                {
+                    connection.Open();
+                    using (SqlDataAdapter DA = new SqlDataAdapter(srQuery, connection))
+                    {
+                        DA.Fill(dtReturnTable);
+                    }
+                }
+            }
+            catch (Exception E)
+            {
+                logSQLErrors(srQuery, E);
+            }
+
+            return dtReturnTable;
+        }
+
+        public static int updateDeleteInsert(string srQuery)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(srConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(srQuery, connection))
+                    {
+                        return command.ExecuteNonQuery();//ExecuteNonQuery because we will not get any results , used for update delete and insert
+                    }
+                }
+            }
+            catch (Exception E)
+            {
+                logSQLErrors(srQuery, E);
+            }
+            return -1;
+        }
+
+        private static void logSQLErrors(string srQuery, Exception E)
+        {
+            File.AppendAllText("SQLerrors.txt", "SQL error: Query= " + srQuery + " Error: " + E?.Message + "\r\n" + E?.InnerException?.ToString() + "\r\n\r\n");
+        }
+
+    }
+}
---
+++ lecture 8/lecture 7/HashandDictionary.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace lecture_7
+{
+    /// <summary>
+    /// Interaction logic for HashandDictionary.xaml
+    /// </summary>
+    public partial class HashandDictionary : Window
+    {
+        public HashandDictionary()
+        {
+            InitializeComponent();
+        }
+
+        Dictionary<string, double> dicTest = new Dictionary<string, double>();
+        List<KeyValuePair<string, double>> listKeyValuePair = new List<KeyValuePair<string, double>>();
+        List<Tuple<string, double>> listTuple = new List<Tuple<string, double>>();
+        HashSet<KeyValuePair<string, double>> hashSetKeyValue = new HashSet<KeyValuePair<string, double>>();
+
+        private void btnTest_Click(object sender, RoutedEventArgs e)
+        {
+            Stopwatch swTimer = new Stopwatch();
+
+            swTimer.Start();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                var vrUId = generateID();
+                if(!dicTest.ContainsKey(vrUId))
+                {
+                    dicTest.Add(vrUId,1);
+                }
+                else
+                {
+                    dicTest[vrUId]++;
+                }
+            }
+        }
+
+        public string generateID()
+        {
+            var vrUId = Guid.NewGuid().ToString("N");
+            return vrUId.Substring(0,2);
+        }
+    }
+}
