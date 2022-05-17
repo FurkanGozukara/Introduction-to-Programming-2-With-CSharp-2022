9e11f28a9108e8486d43c76f2026fd8576479866
Furkan Gözükara <furkangozukara@gmail.com>
2022-05-17 14:29:23 +0300

    lecture 10 part 1

---
+++ lecture 10/lecture 7/HashandDictionary.xaml.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+using System.Threading;
+
+namespace lecture_7
+{
+    /// <summary>
+    /// Interaction logic for HashandDictionary.xaml
+    /// </summary>
+    public partial class HashandDictionary : Window
+    {
+        public HashandDictionary()
+        {
+            InitializeComponent();
+            Debug.WriteLine($"main window thread id : {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        public class keysAndValues
+        {
+            public keysAndValues(string _srKey, double _dblValue)
+            {
+                srKey = _srKey;
+                dblValue= _dblValue;
+            }
+
+            public string srKey { get; set; }
+            public double dblValue { get; set; }
+        }
+
+        Dictionary<string, double> dicTest = new Dictionary<string, double>();
+        List<KeyValuePair<string, double>> listKeyValuePair = new List<KeyValuePair<string, double>>();
+        List<Tuple<string, double>> listTuple = new List<Tuple<string, double>>();
+        HashSet<KeyValuePair<string, double>> hashSetKeyValue = new HashSet<KeyValuePair<string, double>>();
+
+        List<keysAndValues> lstMyCustomClass = new List<keysAndValues>();
+
+        private void btnTest_Click(object sender, RoutedEventArgs e)
+        {
+            int irHowMany = 100000;
+
+            Stopwatch swTimer = new Stopwatch();
+
+            swTimer.Start();
+
+            for (int i = 0; i < irHowMany; i++)
+            {
+                var vrUId = generateID();
+                if (!dicTest.ContainsKey(vrUId))
+                {
+                    dicTest.Add(vrUId, 1);
+                }
+                else
+                {
+                    dicTest[vrUId]++;
+                }
+            }
+
+            swTimer.Stop();
+
+            lstBoxResults.Items.Add("dictionary value generation took: " + swTimer.ElapsedMilliseconds.ToString("N0") + " miliseconds");
+
+            //stringOperations(dicTest);
+
+            swTimer.Restart();
+
+            for (int i = 0; i < irHowMany; i++)
+            {
+                var vrUId = generateID();
+
+                if (listKeyValuePair.Where(kvp => kvp.Key == vrUId).Count() == 0)
+                {
+                    listKeyValuePair.Add(new KeyValuePair<string, double>(vrUId, 1));
+                }
+                else
+                {
+                    for (int index_of_key = 0; index_of_key < listKeyValuePair.Count; index_of_key++)
+                    {
+                        if (listKeyValuePair[index_of_key].Key == vrUId)
+                            listKeyValuePair[index_of_key] = new KeyValuePair<string, double>(listKeyValuePair[index_of_key].Key, (listKeyValuePair[index_of_key].Value + 1));
+                    }
+                }
+            }
+
+            swTimer.Stop();
+
+            lstBoxResults.Items.Add("listKeyValuePair value generation took: " + swTimer.ElapsedMilliseconds.ToString("N0") + " miliseconds");
+
+
+            swTimer.Restart();
+
+            for (int i = 0; i < irHowMany; i++)
+            {
+                var vrUId = generateID();
+
+                if (listTuple.Where(kvp => kvp.Item1 == vrUId).Count() == 0)
+                {
+                    listTuple.Add(new Tuple<string, double>(vrUId, 1));
+                }
+                else
+                {
+                    for (int index_of_key = 0; index_of_key < listTuple.Count; index_of_key++)
+                    {
+                        if (listTuple[index_of_key].Item1 == vrUId)
+                            listTuple[index_of_key] = new Tuple<string, double>(vrUId, listTuple[index_of_key].Item2 + 1);
+
+                        //listTuple.RemoveAt(index_of_key);
+                        //index_of_key--;
+                    }
+                }
+            }
+
+            swTimer.Stop();
+
+            lstBoxResults.Items.Add("listTuple value generation took: " + swTimer.ElapsedMilliseconds.ToString("N0") + " miliseconds");
+
+
+            swTimer.Restart();
+
+            for (int i = 0; i < irHowMany; i++)
+            {
+                var vrUId = generateID();
+
+                if (lstMyCustomClass.Where(kvp => kvp.srKey == vrUId).Count() == 0)
+                {
+                    lstMyCustomClass.Add(new keysAndValues(vrUId,1));
+                }
+                else
+                {
+                    lstMyCustomClass.Where(pr => pr.srKey == vrUId).FirstOrDefault().dblValue++;
+                }
+            }
+
+            swTimer.Stop();
+
+            lstBoxResults.Items.Add("lstMyCustomClass value generation took: " + swTimer.ElapsedMilliseconds.ToString("N0") + " miliseconds");
+
+            swTimer.Restart();
+
+            for (int i = 0; i < irHowMany; i++)
+            {
+                var vrUId = generateID();
+
+                bool blFound = false;
+
+                foreach (var vrPerValue in hashSetKeyValue)
+                {
+                    if(vrPerValue.Key==vrUId)
+                    {
+                        hashSetKeyValue.Remove(vrPerValue);
+                        hashSetKeyValue.Add(new KeyValuePair<string, double>(vrUId, (vrPerValue.Value+1)));
+                        blFound = true;
+                        break;
+                    }
+                }
+
+                if (blFound == false)
+                    hashSetKeyValue.Add(new KeyValuePair<string, double>(vrUId, 1));
+            }
+
+            swTimer.Stop();
+
+            lstBoxResults.Items.Add("hashSetKeyValue value generation took: " + swTimer.ElapsedMilliseconds.ToString("N0") + " miliseconds");
+
+
+            writeListKeyValue(listKeyValuePair);
+        }
+
+        private void writeListKeyValue(List<KeyValuePair<string, double>> varList)
+        {
+            StringBuilder sbDictionary = new StringBuilder();
+
+            foreach (var vrPerItem in varList)
+            {
+                sbDictionary.AppendLine($"Key: {vrPerItem.Key} \t\t Value: {vrPerItem.Value}");
+            }
+
+            File.WriteAllText("list_key_Value.txt", sbDictionary.ToString());
+        }
+
+        private void stringOperations(Dictionary<string, double> dicTest)
+        {
+            Stopwatch swTimer = new Stopwatch();
+
+            dicTest = dicTest.OrderBy(kvp => kvp.Key).ToDictionary(pr => pr.Key, pr => pr.Value);
+
+
+
+            swTimer.Restart();
+
+            StringBuilder sbDictionary = new StringBuilder();
+
+            foreach (var vrPerItem in dicTest)
+            {
+                sbDictionary.AppendLine($"Key: {vrPerItem.Key} \t\t Value: {vrPerItem.Value}");
+            }
+
+            swTimer.Stop();
+
+            File.WriteAllText("dictionary_Values.txt", sbDictionary.ToString());
+
+            lstBoxResults.Items.Add("string buildier methodoloy took: " + swTimer.ElapsedMilliseconds.ToString("N0") + " miliseconds");
+
+            string srResult = "";
+
+            swTimer.Restart();
+
+            foreach (var vrPerItem in dicTest)
+            {
+                //   srResult = srResult + $"Key: {vrPerItem.Key} \t\t Value: {vrPerItem.Value}\r\n";
+            }
+
+            swTimer.Stop();
+
+            File.WriteAllText("dictionary_Values_string_method.txt", srResult);
+
+            lstBoxResults.Items.Add("string methodoloy took: " + swTimer.ElapsedMilliseconds.ToString("N0") + " miliseconds");
+        }
+
+        public string generateID()
+        {
+            var vrUId = Guid.NewGuid().ToString("N");
+            return vrUId.Substring(0, 20);
+        }
+
+        HashSet<string> hsUids = new HashSet<string>();
+        List<string> lstUids = new List<string>();
+        Dictionary<string, string> dicUids = new Dictionary<string, string>();
+
+        private void btnSpeedtest_Click(object sender, RoutedEventArgs e)
+        {
+
+            Debug.WriteLine($"btnSpeedtest_Click click thread id : {Thread.CurrentThread.ManagedThreadId}");
+
+            Task.Factory.StartNew(() => {
+
+                Debug.WriteLine($"inside of Task.Factory.StartNew: {Thread.CurrentThread.ManagedThreadId}");
+                doSpeedTask(); });
+
+
+        }
+
+       
+
+        private void doSpeedTask()
+        {
+            Debug.WriteLine($"doSpeedTask click thread id : {Thread.CurrentThread.ManagedThreadId}");
+
+            int irCounter = 30000;
+
+            Stopwatch swTimer = new Stopwatch();
+
+            for (int i = 0; i < irCounter; i++)
+            {
+                var vrUid = generateID();
+                hsUids.Add(vrUid);
+            }
+
+            swTimer.Stop();
+
+            insertListBoxResult("hashset unique adding took: " + swTimer.ElapsedMilliseconds.ToString("N0") + " miliseconds");
+     
+            swTimer.Restart();
+
+            for (int i = 0; i < irCounter; i++)
+            {
+                var vrUid = generateID();
+                if (hsUids.Contains(vrUid))
+                {
+                    vrUid = "1";
+                }
+            }
+
+            swTimer.Stop();
+
+            insertListBoxResult("hashset searching took: " + swTimer.ElapsedMilliseconds.ToString("N0") + " miliseconds");
+
+            hsUids = new HashSet<string>();
+
+            swTimer.Restart();
+
+            for (int i = 0; i < irCounter; i++)
+            {
+                var vrUid = generateID();
+                lstUids.Add(vrUid);
+            }
+
+            lstUids = lstUids.Distinct().ToList();
+
+            swTimer.Stop();
+
+            insertListBoxResult("list unique adding took: " + swTimer.ElapsedMilliseconds.ToString("N0") + " miliseconds");
+
+
+            swTimer.Restart();
+
+            for (int i = 0; i < irCounter; i++)
+            {
+                var vrUid = generateID();
+                if (lstUids.Contains(vrUid))
+                {
+                    vrUid = "1";
+                }
+            }
+
+            swTimer.Stop();
+
+            insertListBoxResult("list searching took: " + swTimer.ElapsedMilliseconds.ToString("N0") + " miliseconds");
+
+            lstUids = new List<string>();
+
+            swTimer.Restart();
+
+            for (int i = 0; i < irCounter; i++)
+            {
+                var vrUid = generateID();
+                if (dicUids.ContainsKey(vrUid) == false)
+                    dicUids.Add(vrUid, null);
+            }
+
+            swTimer.Stop();
+
+            insertListBoxResult("dictionary unique adding took: " + swTimer.ElapsedMilliseconds.ToString("N0") + " miliseconds");
+
+            swTimer.Restart();
+
+            for (int i = 0; i < irCounter; i++)
+            {
+                var vrUid = generateID();
+                if (dicUids.ContainsKey(vrUid) == true)
+                {
+                    vrUid = "1";
+                }
+            }
+
+            swTimer.Stop();
+
+            insertListBoxResult("dictionary searching took: " + swTimer.ElapsedMilliseconds.ToString("N0") + " miliseconds");
+        }
+
+        private void insertListBoxResult(string srMsg)
+        {
+            Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal, () => {
+
+                lstBoxResults.Items.Add(srMsg);
+            });
+        }
+    }
+}
---
+++ lecture 10/lecture 7/MainWindow.xaml.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Globalization;
+using System.Threading;
+using System.IO;
+
+namespace lecture_7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("tr-TR");
+            CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("tr-TR");
+            // Put the following code before InitializeComponent()
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("tr-TR");
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("tr-TR");
+            FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(System.Windows.Markup.XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+
+            lstFirstNames = File.ReadAllLines("first-names.txt").ToList();
+            lstLastNames = File.ReadAllLines("last-names.txt").ToList();
+        }
+
+        private void btnLoadDataGrid_Click(object sender, RoutedEventArgs e)
+        {
+            refreshDataGrid();
+        }
+
+        int irPageSize = 100;
+
+        int irCountOfPages;
+
+        int irPrevPageNumber, irNextPageNumber;
+
+        private void refreshDataGrid()
+        {
+            int irSelectedIndex = cbmPages.SelectedIndex;
+
+            int irRecordcount = DbOperations.selectTable("select COUNT (*) from tblStudents").Rows[0][0].ToString().toInt32();
+
+            irCountOfPages = irRecordcount / irPageSize + 1;
+
+            cbmPages.Items.Clear();
+            for (int i = 1; i < irCountOfPages+1; i++)
+            {
+                cbmPages.Items.Add("Page: " + i);
+            }
+            cbmPages.SelectedIndex = irSelectedIndex;
+            int irCurrentRecordPage = irSelectedIndex+1 ;
+
+            if (irCurrentRecordPage < 1)
+                irCurrentRecordPage = 1;
+
+            irPrevPageNumber = ((irCurrentRecordPage < 2) ? irCountOfPages : irCurrentRecordPage - 1);
+
+            irNextPageNumber = ((irCurrentRecordPage == irCountOfPages) ? 1 : irCurrentRecordPage + 1);
+
+            btnPrev.Content = "Goto Page: " + irPrevPageNumber;
+            btnNext.Content = "Goto Page: " + irNextPageNumber;
+            lblCurrentPage.Content = irCurrentRecordPage;
+
+            string srQuery = $@"DECLARE @PageNumber AS INT
+DECLARE @RowsOfPage AS INT
+SET @PageNumber={irCurrentRecordPage}
+SET @RowsOfPage={irPageSize}
+SELECT * FROM tblStudents
+ORDER BY StudentId 
+OFFSET (@PageNumber-1)*@RowsOfPage ROWS
+FETCH NEXT @RowsOfPage ROWS ONLY";
+
+            DataTable dtData = DbOperations.selectTable(srQuery);
+
+            //for (int i = 0; i < dtData.Rows.Count; i++)
+            //{
+            //    var vrVal = dtData.Rows[i]["BirthDate"].ToString();
+            //    if (string.IsNullOrEmpty(vrVal.Trim()))
+            //        continue;
+            //    dtData.Rows[i]["BirthDate"] = Convert.ToDateTime(dtData.Rows[i]["BirthDate"].ToString()).ToString("yyyy-MM-dd");
+            //}
+
+            DataView dvData = new DataView(dtData);
+
+
+            dtStudents.ItemsSource = dvData;
+          
+
+
+        }
+
+        private void btnSaveNewStudent_Click(object sender, RoutedEventArgs e)
+        {
+            string srAddNewStudent = @"insert into tblStudents (StudentId,StudentName,PhoneNumber,Email,BirthDate,LastLoginDate) 
+                values ({0},N'{1}',N'{2}',N'{3}',N'{4}',N'{5}')";
+
+            srAddNewStudent = string.Format(srAddNewStudent,
+                txtStudentId.Text,
+                txtStudentName.Text,
+                txtPhoneNumber.Text,
+                txtEmail.Text,
+                dateBirth.SelectedDate.Value.ToString(),
+                dateLogin.SelectedDate.Value.ToString());
+
+            int irResult = DbOperations.updateDeleteInsert(srAddNewStudent);
+            displayResult(irResult);
+        }
+
+        private void displayResult(int irResult)
+        {
+            if (irResult == -1)
+            {
+                MessageBox.Show("an SQL error occured. Please check the logs");
+                return;
+            }
+
+            MessageBox.Show("success query. number of rows affected : " + irResult);
+
+            refreshDataGrid();
+        }
+
+        private void btnDeleteSelected_Click(object sender, RoutedEventArgs e)
+        {
+            if (dtStudents.SelectedItem == null)
+                return;
+
+            DataRowView vrItem = (DataRowView)dtStudents.SelectedItem;
+
+            MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show($"Are you sure to delete Student Id {vrItem.Row["StudentId"]} - {vrItem.Row["StudentName"]}?", "Delete Confirmation", System.Windows.MessageBoxButton.YesNo);
+            if (messageBoxResult != MessageBoxResult.Yes)
+            {
+                return;
+            }
+
+            int irResult = DbOperations.updateDeleteInsert($"delete from tblStudents where StudentId={vrItem.Row["StudentId"]}");
+
+            displayResult(irResult);
+        }
+
+        private void btnUpdateRecord_Click(object sender, RoutedEventArgs e)
+        {
+            if (dtStudents.SelectedItem == null)
+                return;
+
+            DataRowView vrItem = (DataRowView)dtStudents.SelectedItem;
+
+            MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show($"Are you sure to update Student Id {vrItem.Row["StudentId"]} - {vrItem.Row["StudentName"]}?", "Delete Confirmation", System.Windows.MessageBoxButton.YesNo);
+            if (messageBoxResult != MessageBoxResult.Yes)
+            {
+                return;
+            }
+
+            DateTime dtBirth = Convert.ToDateTime(vrItem["BirthDate"]);
+            DateTime dtLastLogin = Convert.ToDateTime(vrItem["LastLoginDate"]);
+
+            int irResult = DbOperations.updateDeleteInsert(@$"  update tblStudents set StudentName=N'{vrItem["StudentName"]}',PhoneNumber=N'{vrItem["PhoneNumber"]}',Email=N'{vrItem["Email"]}',BirthDate=N'{dtBirth.ToString("yyyy-MM-dd")}',LastLoginDate=N'{dtLastLogin.ToString("yyyy-MM-dd hh:mm:ss.000")}' 
+                    where StudentId = {vrItem["StudentId"]}");
+
+            displayResult(irResult);
+        }
+
+        private void btnRandomStudent_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Factory.StartNew(() => { generateRandomStudents(); });
+        }
+
+        private void generateRandomStudents()
+        {
+            string srBaseQuery = "insert into tblStudents (StudentName,PhoneNumber,Email,BirthDate) values (N'{0}',N'{1}',N'{2}',N'{3}')";
+
+            HashSet<string> hsUsedEmails = new HashSet<string>();
+
+            foreach (DataRow drw in DbOperations.selectTable("select Email from tblStudents").Rows)
+            {
+                hsUsedEmails.Add(drw[0].ToString());
+            }
+
+            StringBuilder srQueries = new StringBuilder();
+
+            int irCounter2 = 1;
+
+            for (int i = 0; i < 100000; i++)
+            {
+                string srName = generateRandomName();
+                string srPhone = generateRandomPhoneNumber();
+                string srEmail = srName.Replace(" ", "_") + returnEmailProvider();
+                while (true)
+                {
+                    int irCounter = 0;
+                    if (hsUsedEmails.Contains(srEmail))
+                    {
+                        srEmail = srName.Replace(" ", "_") + irCounter + returnEmailProvider();
+                        irCounter++;
+                    }
+                    else
+                    {
+                        hsUsedEmails.Add(srEmail);
+                        break;
+                    }
+                }
+                string srBirthDate = generateRandomBirthdate();
+
+                string srFormattedQuery = string.Format(srBaseQuery, srName, srPhone, srEmail, srBirthDate);
+
+                srQueries.Append(srFormattedQuery + ";");
+
+                //  DbOperations.updateDeleteInsert(srFormattedQuery);
+
+                if (irCounter2 > 250)
+                {
+                    DbOperations.updateDeleteInsert(srQueries.ToString());
+                    srQueries = new StringBuilder();
+                    irCounter2 = 1;
+                }
+                irCounter2++;
+                Dispatcher.BeginInvoke((Action)(() =>
+                {
+                    lblProgress.Content = "so far processed student count: " + i.ToString("N0");
+                }));
+              
+            }
+
+            DbOperations.updateDeleteInsert(srQueries.ToString());
+        }
+
+        static List<string> lstProviders = new List<string> { "gmail", "outlook", "hotmail", "yahoo", "dmail", "yandex", "live" };
+
+        private static string returnEmailProvider()
+        {
+            return "@" + lstProviders[myRand.Next(0, lstProviders.Count)] + ".com";
+        }
+
+
+        static List<string> lstFirstNames = new List<string>();
+        static List<string> lstLastNames = new List<string>();
+
+        private static Random myRand = new Random();
+
+        private static string generateRandomName()
+        {
+            return lstFirstNames[myRand.Next(0, lstFirstNames.Count)] + " " + lstLastNames[myRand.Next(0, lstLastNames.Count)];
+        }
+
+        private static string generateRandomPhoneNumber()
+        {
+            //+90-5**-***-**-**
+            return $"+90-5{myRand.Next(0, 10)}{myRand.Next(0, 10)}-{myRand.Next(0, 10)}{myRand.Next(0, 10)}{myRand.Next(0, 10)}-{myRand.Next(0, 10)}{myRand.Next(0, 10)}-{myRand.Next(0, 10)}{myRand.Next(0, 10)}";
+        }
+
+        private static string generateRandomBirthdate()
+        {
+            //1900-12-12
+            return $"19{myRand.Next(0, 10)}{myRand.Next(0, 10)}-{myRand.Next(1, 13)}-{myRand.Next(1, 29)}";
+        }
+
+        private void btnOpenHashSet_Click(object sender, RoutedEventArgs e)
+        {
+            HashandDictionary newWindow = new HashandDictionary();
+            newWindow.Show();
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            cbmPages.SelectedIndex = irNextPageNumber-1;
+            refreshDataGrid();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Are you sure to delete all existing records?", "Delete Confirmation", System.Windows.MessageBoxButton.YesNo);
+            if (messageBoxResult == MessageBoxResult.No)
+                return;
+
+                DbOperations.updateDeleteInsert("truncate table tblstudents;");
+            DbOperations.updateDeleteInsert("DBCC CHECKIDENT(tblstudents, RESEED, 1)");
+        }
+
+        private void btnPrev_Click(object sender, RoutedEventArgs e)
+        {
+            cbmPages.SelectedIndex = irPrevPageNumber-1;
+            refreshDataGrid();
+        }
+    }
+}
