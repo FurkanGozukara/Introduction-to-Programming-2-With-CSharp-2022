53c971bf9071d8da9207d0086824a7a0f1573dfa
Furkan Gözükara <furkangozukara@gmail.com>
2022-05-24 13:47:16 +0300

    lecture v11 v2

--- lecture 11/lecture 7/obj/Debug/net6.0-windows/HashandDictionary.g.i.cs
+++ lecture 11/lecture 7/obj/Debug/net6.0-windows/HashandDictionary.g.i.cs
@@ -77,7 +77,7 @@ namespace lecture_7 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/lecture 11;V1.0.0.0;component/hashanddictionary.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/lecture 11;component/hashanddictionary.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\HashandDictionary.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
